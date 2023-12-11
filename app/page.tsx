aa3668ffd7caaeb8d2dd08e6930e1b0b541005f1
night-sornram <sornram.chalernma@gmail.com>
2023-12-12 02:10:03 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,55 @@
 import Image from 'next/image'
 
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=' w-screen h-screen flex flex-col justify-start items-center'>
+      <div className=' w-11/12 md:w-2/3 mt-10  text-center '>
+        <div className='text-3xl md:text-5xl font-extralight text-custom-100 mt-5'>Reliable, efficient delivery</div>
+        <div className='text-3xl md:text-5xl  text-custom-100 font-bold mt-5'>Powered by Technology</div>
+        <div className='text-base mt-5 mx-0 md:mx-24 md:text-lg text-custom-200'> Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</div>
+      </div>
+      <div className=' md:mw-11/12 lg:mw-10/12 md:pb-0 pb-20 flex flex-col md:flex-row m-10 justify-center items-center'>
+        <div className=' h-72 w-full md:w-1/3 relative p-8 rounded-lg border-solid border-t-4 shadow-xl border-c-100'>
+          <div className=' text-lg text-custom-100 font-bold'>
+            Supervisor
+          </div>
+          <div className=' text-custom-200'>
+            Monitors activity to identify project roadblocks
+          </div>
+          <img src="/images/icon-supervisor.svg" alt="supervisor" width={64} height={64} className=' absolute bottom-10 right-10' />
+        </div>
+        <div className=' flex flex-col w-full md:w-1/3 mx-0 md:mx-10'>
+          <div className='h-72 mt-10 md:mt-0 relative p-8 rounded-lg border-solid border-t-4 shadow-xl border-r-100'>
+            <div className=' text-lg text-custom-100 font-bold'>
+              Team Builder
+            </div>
+            <div className=' text-custom-200'>
+              Scans our talent network to create the optimal team for your project
+            </div>
+            <img src="/images/icon-team-builder.svg" alt="team-builder" width={64} height={64} className=' absolute bottom-10 right-10' />
+          </div>
+          <div className='h-72 relative p-8 mt-10 rounded-lg border-solid border-t-4 shadow-xl border-o-100' >
+            <div className=' text-lg text-custom-100 font-bold'>
+              Karma
+            </div>
+            <div className=' text-custom-200'>
+              Regularly evaluates our talent to ensure quality
+            </div>
+            <img src="/images/icon-karma.svg" alt="karma" width={64} height={64} className=' absolute bottom-10 right-10' />
+        </div>
+        </div>
+        <div className='h-72 w-full mt-10 md:mt-0 md:w-1/3 relative p-8 rounded-lg border-solid border-t-4 shadow-xl border-b-100' >
+          <div className=' text-lg text-custom-100 font-bold'>
+            Calculator
+          </div>
+          <div className=' text-custom-200'>
+            Uses data from past projects to provide better delivery estimates
+          </div>
+          <img src="/images/icon-calculator.svg" alt="calculator" width={64} height={64} className=' absolute bottom-10 right-10' />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    </div>
+     )}
+   
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,31 @@
 import type { Config } from 'tailwindcss'
 
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+
+const colors = {
+    ...defaultColors,
+    ...{
+        "custom": {
+            "100": "hsl(234, 12%, 34%)",
+            "200" : "hsl(229, 6%, 66%)",
+            "300" : "hsl(0, 0%, 98%)"
+        },
+        "c" :{
+          "100" : "hsl(180, 62%, 55%)",
+        },
+        "r" :{
+          "100" : "hsl(0, 78%, 62%)",
+        },
+        "b" :{
+          "100" : "hsl(212, 86%, 64%)",
+        },
+        "o" :{
+          "100" : "hsl(34, 97%, 64%)",
+        },
+    },
+}
+
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,6 +39,7 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors : colors
     },
   },
   plugins: [],
